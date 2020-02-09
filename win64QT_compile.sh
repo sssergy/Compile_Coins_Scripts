431b15e8ad094ef6bb4bfabcb2d9768f1c9366e9
sssergy <admin@rem05.ru>
2020-02-09 22:32:10 +0300

    Update win64QT_compile.sh

--- win64QT_compile.sh
+++ win64QT_compile.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-sudo make clean
+make clean
 chmod 777 -R *
 PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
 cd `pwd`/depends
-sudo make HOST=x86_64-w64-mingw32 -j 8
+make HOST=x86_64-w64-mingw32 -j 8
 cd ..
-sudo ./autogen.sh
+./autogen.sh
 mkdir db4
 wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
 tar -xzvf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix/
 ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
-sudo make install
+make -j8
 cd ../../
 ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-w64-mingw32 --enable-tests=no --enable-bench=no
-sudo make -j 8
+make -j 8
 strip src/*.exe
 strip src/qt/*.exe
