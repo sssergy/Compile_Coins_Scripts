46476f81f34fd3a7edd0e98e84f033aa32f8f0a9
sssergy <admin@rem05.ru>
2020-02-09 22:27:47 +0300

    disable bench

--- win64QT_compile.sh
+++ win64QT_compile.sh
@@ -3,7 +3,7 @@ sudo make clean
 chmod 777 -R *
 PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
 cd `pwd`/depends
-sudo make HOST=x86_64-w64-mingw32 -j 12
+sudo make HOST=x86_64-w64-mingw32 -j 8
 cd ..
 sudo ./autogen.sh
 mkdir db4
@@ -13,7 +13,7 @@ cd db-4.8.30.NC/build_unix/
 ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
 sudo make install
 cd ../../
-./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-w64-mingw32 --enable-tests=no
-sudo make -j 12
+./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-w64-mingw32 --enable-tests=no --enable-bench=no
+sudo make -j 8
 strip src/*.exe
 strip src/qt/*.exe
