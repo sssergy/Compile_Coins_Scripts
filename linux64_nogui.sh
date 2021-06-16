a3495cfef21ba7bd182dad6aeb18455d524f8df6
sssergy <admin@rem05.ru>
2021-06-16 14:06:26 +0300

    Update linux64_nogui.sh

--- linux64_nogui.sh
+++ linux64_nogui.sh
@@ -2,7 +2,7 @@
 make clean
 chmod 777 -R *
 cd `pwd`/depends
-make -j6
+make -j24
 cd ..
 ./autogen.sh
 mkdir `pwd`/db4
@@ -13,6 +13,6 @@ cd `pwd`/db-4.8.30.NC/build_unix/
 make install
 cd ../../
 ./autogen.sh
-./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu --enable-tests=no
-make -j6
+./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu --enable-tests=no --enable-bench=no --with-gui=yes
+make -j24
 echo "Remember to strip the daemon, cli, and tx files!"
